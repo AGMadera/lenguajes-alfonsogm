5c2b6a86730c899ccfb945a40ae31e371ec0e990
T-107 <T-107@PC257>
2015-05-28 19:42:31 -0500

    Clase 28/mayo/2015

--- src/abstraccion/Imc.java
+++ src/abstraccion/Imc.java
@@ -10,16 +10,50 @@ package abstraccion;
  * @author T-107
  */
 public class Imc {
-    float peso;
-    float altura;
-      
- public Imc(float p, float a){
- peso=p;
- altura=a;
- }
-    
-    public String calcular(){
-        float imc= peso/(altura*altura);
-        return "Tu imc es: "+imc;
+  private float peso;
+  private  float altura;
+  private boolean flaco;
+  
+  public Imc(/*float p, float a*/){
+  //peso=p;
+  //altura=a;
+  
+  
+  }
+
+
+    public void setPeso(float peso) {
+        this.peso = peso;
     }
+
+ 
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    public float getPeso() {
+        return peso;
+    }
+    
+    public float getAltura() {
+        return altura;
+    }
+    /*
+    public float calcularImc(float altura, float peso){
+        return calculo=peso / (altura*altura);
+    }*/
+    
+    public float calcular(){
+        float calcular=peso/(altura*altura);
+        return calcular;
+    }
+    
+    public void setFlaco(boolean flaco){
+        this.flaco=flaco;
+    }
+    
+    public boolean isFlaco(){
+        return flaco;
+    }
+    
 }
--- src/abstraccion/InterfazImc.java
+++ src/abstraccion/InterfazImc.java
@@ -12,9 +12,32 @@ package abstraccion;
 public class InterfazImc{  
 
 public static void main(String oso[]){
- Imc imc=new Imc(80, 1.67f);
+ Imc imc=new Imc(/*84,1.67f*/);
+ imc.setPeso(90);
  
  System.out.println(imc.calcular());
+ System.out.println(imc.getPeso());
+ 
+ ////primitivos integrales
+ byte b=2;
+ short s=2;
+ int i=2;
+ long l=2;
+ 
+ b=(byte)s;
+ s=b;
+ i=(int)l;
+ i=s;
+ s=(short)i;
+ 
+ float f=(float)2.0;
+ double d=2.0;
+ 
+ boolean valor =false;
+ Imc otro=new Imc();
+ otro.setFlaco(valor);
+ System.out.println(otro.isFlaco());
+ 
  
 }
 
