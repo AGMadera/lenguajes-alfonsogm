a690ad4d21d96b694e2400f0c0780e76d849422b
AGMadera <ponchp.agm@gmail.com>
2015-06-15 19:43:37 -0500

    Clase 15 de Junio Herencia

---
+++ src/MiPrimeraExcepcion/MenordeEdadException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MiPrimeraExcepcion;
+
+/**
+ *
+ * @author alfonsogalvanmadera
+ */
+public class MenordeEdadException extends Exception{
+    
+    public MenordeEdadException(){
+    super("Eres un chamaco pervertido, le dire a tu mamá!!, no te puedo registrar");
+    }
+    
+    
+}
