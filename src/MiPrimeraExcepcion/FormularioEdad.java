193231c640f3b0ed676c34e619a587a76d4e4a94
AGMadera <ponchp.agm@gmail.com>
2015-06-18 19:43:05 -0500

    Clase 18 de Junio Herencia

--- src/MiPrimeraExcepcion/FormularioEdad.java
+++ src/MiPrimeraExcepcion/FormularioEdad.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Validacion minimo 4 caracteres, maximo 10
  */
 package MiPrimeraExcepcion;
 
