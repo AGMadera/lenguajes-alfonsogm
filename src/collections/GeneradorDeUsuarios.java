057830ffaf3ae1b909dedaadd7c5c4e31ff83579
AGMadera <ponchp.agm@gmail.com>
2015-07-13 19:43:27 -0500

    Clase 13 de Julio

--- src/collections/GeneradorDeUsuarios.java
+++ src/collections/GeneradorDeUsuarios.java
@@ -22,7 +22,12 @@ public class GeneradorDeUsuarios {
         Usuario u3=new Usuario("ana", 38, "ana@outlook.com");
         Usuario u4=new Usuario("pedro", 32, "pedro@yahoo.com");
         
+        
         usuarios =new ArrayList<Usuario>();
+        usuarios.add(u1);
+        usuarios.add(u2);
+        usuarios.add(u3);
+        usuarios.add(u4);
     }
 
     public List<Usuario> getUsuarios() {
@@ -31,6 +36,13 @@ public class GeneradorDeUsuarios {
 
     public void setUsuarios(List<Usuario> usuarios) {
         this.usuarios = usuarios;
+    }
+    
+    public String agregarUsuario(String nombre, int edad, String email){
+        Usuario u=new Usuario(nombre, edad, email);
+        usuarios.add(u);
+        
+    return "usuario agragado con exito";
     }
     
     
